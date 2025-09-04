2d431311596257c459c2591bfe30fca0d2235708
aronno256 <tahsinahamed@iut-dhaka.edu>
2025-09-05 00:56:08 +0600

    added the main icon

--- src/main/java/com/example/mindash/Main.java
+++ src/main/java/com/example/mindash/Main.java
@@ -3,6 +3,7 @@ package com.example.mindash;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,17 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main_Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        // Set the application icon
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("icon_.jpeg"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("Could not load application icon: " + e.getMessage());
+        }
+
         scene.getStylesheets().add(getClass().getResource("MainMenu.css").toExternalForm());
-        stage.setTitle("Hello!");
+        stage.setTitle("MinDash");
         stage.setScene(scene);
         stage.show();
     }
